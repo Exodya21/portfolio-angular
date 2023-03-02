df318b57cd41979c62db86d06d48bd8ce81a0f90
Jaume <jaumegomiciai@gmail.com>
2023-03-02 14:22:37 +0100

    nav-mobile work true

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,8 +6,20 @@ import { InfoPaginaService } from 'src/app/services/info-pagina.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
+
+
 export class HeaderComponent {
   constructor( public _DB: InfoPaginaService ) {}
 
+  isChecked: any;
 
+  public onChange(event: any): void {
+    // this.isChecked = event.target['checked'];
+    this.isChecked = event;
+  }
+
+  public close() {
+    this.isChecked = this.isChecked.target['checked'] = false;
+  }
 }
